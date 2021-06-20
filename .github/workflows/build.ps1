e9695dee5eb07d36a0c0c27cabac38b7ff697995
Christoph M. Becker <cmbecker69@gmx.de>
2021-06-20 23:33:15 +0200

    ci

---
+++ .github/workflows/build.ps1
@@ -0,0 +1,27 @@
+param (
+    [Parameter(Mandatory)] $vs,
+    [Parameter(Mandatory)] $arch
+)
+
+$ErrorActionPreference = "Stop"
+
+New-Item "winlib_deps" -ItemType "directory"
+
+$temp = New-TemporaryFile | Rename-Item -NewName {$_.Name + ".zip"} -PassThru
+Invoke-WebRequest "https://windows.php.net/downloads/php-sdk/deps/$vs/$arch/libbzip2-1.0.8-$vs-$arch.zip" -OutFile $temp
+Expand-Archive $temp -DestinationPath "winlib_deps"
+
+$temp = New-TemporaryFile | Rename-Item -NewName {$_.Name + ".zip"} -PassThru
+Invoke-WebRequest "https://windows.php.net/downloads/php-sdk/deps/$vs/$arch/liblzma-5.2.5-$vs-$arch.zip" -OutFile $temp
+Expand-Archive $temp -DestinationPath "winlib_deps"
+
+$temp = New-TemporaryFile | Rename-Item -NewName {$_.Name + ".zip"} -PassThru
+Invoke-WebRequest "https://windows.php.net/downloads/php-sdk/deps/$vs/$arch/zlib-1.2.11-$vs-$arch.zip" -OutFile $temp
+Expand-Archive $temp -DestinationPath "winlib_deps"
+
+$platform = if ($arch -eq "x64") {"x64"} else {"Win32"}
+
+New-Item "build" -ItemType "directory"
+Set-Location "build"
+cmake -G "NMake Makefiles" "-DCMAKE_GENERATOR_PLATFORM=$platform" "-DZLIB_INCLUDE_DIR=..\winlib_deps\include" "-DZLIB_LIBRARY=..\winlibs_deps\lib\zlib_a.lib" "-DBZIP2_INCLUDE_DIR=..\winlibs_deps\include" "-DBZIP2_LIBRARIES=..\winlibs_deps\lib\libbz2_a.lib" "-DLIBLZMA_INCLUDE_DIR=..\winlibs_deps\include" "-DLIBLZMA_LIBRARY=..\winlibs_deps\lib\liblzma_a.lib" "-DBUILD_REGRESS=OFF" ".."
+nmake
